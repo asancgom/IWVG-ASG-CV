467a69cef12dadbdcb24ec7a3db852cbe93e44e5
Angela <Angela@192.168.1.102>
2017-09-29 00:27:57 +0200

    Create class C51

---
+++ src/test/java/es/upm/cv/asancgom/C51Test.java
@@ -0,0 +1,19 @@
+package es.upm.cv.asancgom;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class C51Test {
+	private C51 c;
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1", c.m1());
+	}
+	
+	@Test
+	public void testM2() {
+		assertEquals("m2", c.m2());
+	}
+}
