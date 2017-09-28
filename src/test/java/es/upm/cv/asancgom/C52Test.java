97c06649ea45ddbc59ef7f0fe24492a0a2655665
Angela <Angela@192.168.1.102>
2017-09-29 00:38:12 +0200

    Create M52Test

---
+++ src/test/java/es/upm/cv/asancgom/C52Test.java
@@ -0,0 +1,18 @@
+package es.upm.cv.asancgom;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import es.upm.cv.asancgom.C52;
+
+public class C52Test {
+
+	private C52 c;
+	
+	@Test
+	public void testC52() {
+		assertEquals("mA", c.mA());
+		
+	}
+}
